50eb8b3c21a287c0ee4e9c8a3f467634a60ded25
Haneesh Pediredla <haneeshpediredla0509@gmail.com>
2022-05-26 21:01:11 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(5500, () => {
+    console.log("Server is started at post 5500");
+});
