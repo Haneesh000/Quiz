2ab607cc97f9f67f8841d4cfc53b1f14cc146423
Haneesh Pediredla <haneeshpediredla0509@gmail.com>
2022-05-28 12:45:38 +0530

    Updates

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -17,16 +18,44 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/quiz", (req, res) => {
-    res.send("Page under development");
+    res.render("quiz");
 });
 
 app.get("/developer", (req, res) => {
     res.render("developer");
 });
 
+app.post("/quiz", (req, res) => {
+    const value1 = req.body.questiononeblank;
+    const value2 = req.body.questiontwoblank;
+    const value3 = req.body.questionthreeblank;
+    const value4 = req.body.questionfourblank;
+    const value5 = req.body.questionfiveblank;
+
+    var score = 0;
+
+    if(_.lowerCase(value1) == _.lowerCase("is")) {
+        score += 1;
+    }
+    if(_.lowerCase(value2) == _.lowerCase("loves")) {
+        score += 1;
+    }
+    if(_.lowerCase(value3) == "best") {
+        score += 1;
+    }
+    if(_.lowerCase(value4) == "is") {
+        score += 1;
+    }
+    if(_.lowerCase(value5) == _.lowerCase("is coming back")) {
+        score += 1;
+    }
+
+    res.render("results", {marks: score, maxMarks: 5})
+});
+
 app.get("*", (req, res) => {
     res.render("404");
-})
+});
 
 app.listen(5500, () => {
     console.log("Server is started at port 5500");
